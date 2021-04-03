45f72cf16acd7141be76055f31e105de28918f90
Rituj Beniwal <ritujbeniwal@gmail.com>
2021-04-03 12:03:30 +0530

    raphael: Downgrade to soundtrigger 2.2 HAL
    Signed-off-by: Rituj Beniwal <ritujbeniwal@gmail.com>

--- device.mk
+++ device.mk
@@ -52,8 +52,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.soundtrigger@2.2-impl \
-    android.hardware.soundtrigger@2.3-impl
+    android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
